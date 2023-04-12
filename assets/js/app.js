72b274e65eac6affa992f8a4ddad7a5be834e0d5
Joey Takeda <joeytakeda@users.noreply.github.com>
2023-04-12 10:55:59 -0700

    Fix URL

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,5 @@
 var CETEIcean = new CETEI()
-const repo = 'https://raw.githubusercontent.com/brluies/SCRB_AldusDS/main/';
+const repo = 'https://raw.githubusercontent.com/joeytakeda/SCRB_AldusDS/main/';
 const TEI = document.querySelector('html').getAttribute('id');
 const URL = repo + TEI + '.xml';
 CETEIcean.getHTML5(URL, function(data) {
